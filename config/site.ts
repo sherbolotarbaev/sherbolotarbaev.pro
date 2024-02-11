d66b4c7f777b31ad135247e4405c9bf990cc628e
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-02-12 04:26:10 +0600

    Update site config

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   name: "Sherbolot Arbaev",
   description: "Software Development Engineer | Full Stack Developer",
-  url: "https://next-sher.vercel.app",
+  url: "https://sherbolotarbaev.pro",
 };
